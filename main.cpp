cbcaab7cb2cf61eeefa42e8e4cd0ebb0479c1d06
SwampWood <118381294+SwampWood@users.noreply.github.com>
2024-09-06 22:13:17 +0300

    Версия 0.1
    Нерабочая

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include <stdio.h>
+#include <TXLib.h>
 
-using namespace std;
+const int LENGHT_OF_STRING = 80;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void GetWholeText(char text[][LENGHT_OF_STRING], FILE* text_file);
+
+int main() {
+    FILE* onegin_file;
+    onegin_file = fopen("text.txt", "r");
+    char text[][LENGHT_OF_STRING] = {};
+    GetWholeText(text, onegin_file);
     return 0;
+}
+
+void GetWholeText(char text[][LENGHT_OF_STRING], FILE* text_file) {
+    int i = 0;
+    while (fgets(text[i], LENGHT_OF_STRING, text_file) != NULL)
+        ++i;
 }
