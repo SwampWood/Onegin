962bb2f61e72011001e686325ca0222d003272b2
SwampWood <danya.ney@yandex.ru>
2024-09-08 23:15:05 +0300

    0.4
    Всё ещё ничего

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #include <TXLib.h>
 
 const int LENGHT_OF_STRING = 80;
+const char* const FILENAME = "text.txt";
 
 size_t GetWholeText(char text[][LENGHT_OF_STRING], FILE* text_file);
 void PrintResult(char text[][LENGHT_OF_STRING], size_t count_of_strings);
 
 int main() {
     FILE* onegin_file;
-    onegin_file = fopen("text.txt", "r");
+    assert(FILENAME != NULL);
+    onegin_file = fopen(FILENAME, "r");
     char text[][LENGHT_OF_STRING] = {};
     size_t count_of_rows = GetWholeText(text, onegin_file);
     printf("%s", text[0]);
@@ -17,9 +19,10 @@ int main() {
 }
 
 size_t GetWholeText(char text[][LENGHT_OF_STRING], FILE* text_file) {
-//TODO поменять индекс последней строки (возвращаемое значение) на длину текста
+    //TODO поменять индекс последней строки (возвращаемое значение) на длину текста
     size_t len = 0;
     while (fgets(text[len], LENGHT_OF_STRING, text_file) != NULL) {
+        assert(text[len] != NULL);
         ++len;
     }
     return len;
